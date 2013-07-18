64ab474cb15b2923bca722ff2c76d49f482060a6
ltwheeler <ltwheeler@ucdavis.edu>
2013-07-18 15:15:14 -0700

    initial site build and dss_rm yml initializer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DssPortal::Application.config.secret_token = '206efac36f48ee51ad76d2e2df66c545773c238c9d206fa2e92d094a89c49f5491a41145288cfd46a42c4d9efac7292790fb1a4464fb5a31b70620cfd2b3b3e6'
