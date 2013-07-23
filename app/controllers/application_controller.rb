225f90597d690570f9ce1c76b6cb26ec94fa2c79
ltwheeler <ltwheeler@ucdavis.edu>
2013-07-23 03:34:47 -0700

    added logic to record new users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 	before_filter CASClient::Frameworks::Rails::Filter
 	before_filter :set_current_user
+  before_filter :find_or_create_person
+  before_filter :update_rm_assignments
 	protect_from_forgery
   
   def welcome
@@ -20,7 +22,20 @@ def logout
 		@current_user = Authorization.current_user
 		logger.info "#{session[:cas_user]}@#{request.remote_ip}: Set current user to #{Authorization.current_user.inspect}."
 	end
+  
+  def find_or_create_person
+    @person = Person.find_by_loginid(@current_user)
+    if @person == nil
+      @rmperson = RmPerson.find(@current_user)
+      @person = Person.new
+      @person.name = @rmperson.name
+      @person.loginid = @rmperson.loginid
+    end
+  end
 
+  def update_rm_assignments
+  
+  end
 
 	# def current_user
 	#   Authorization.current_user
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,14 +1,7 @@
-class Person < ActiveResource::Base
-	self.site = $DSS_RM_SETTINGS['API_KEY_HOST']
-	self.user = $DSS_RM_SETTINGS['API_KEY_USER']
-	self.password = $DSS_RM_SETTINGS['API_KEY_PASSWORD']
-	self.element_name = "person"
+class Person < ActiveRecord::Base
+  has_many :rm_application_assignments
+  has_many :bookmark_assignments
+  has_many :applications
+  accepts_nested_attributes_for :applications, :rm_application_assignments, :bookmark_assignments
+  attr_accessible :loginid, :name
 end
-
-#class Person < ActiveRecord::Base
-#  has_many :rm_application_assignments
-#  has_many :bookmark_assignments
-#  has_many :applications
-#  accepts_nested_attributes_for :applications
-#  attr_accessible :loginid, :name
-#end
