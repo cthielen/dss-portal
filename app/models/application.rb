78ea3eba6170a6df54c133a3dbaaf350dbd4eee5
ltwheeler <ltwheeler@ucdavis.edu>
2013-07-23 08:09:20 -0700

    fixed naming discrepancies between schema/model/view on applications

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ActiveRecord::Base
   belongs_to :person
   belongs_to :category, polymorphic: true
-  attr_accessible :favorite, :person_id, :position, :category_type, :type_id
+  attr_accessible :favorite, :person_id, :position, :category_type, :category_id
 end
