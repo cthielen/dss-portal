05da142e99efce6214ed3b5bf88250ee78a2be1b
ltwheeler <ltwheeler@ucdavis.edu>
2013-08-13 04:27:26 -0700

    added logic to harvest app layout to be recorded

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -28,7 +28,29 @@ $(window).load(function()
 	forcePlaceholderSize: true,
     stop: function(event, ui) 
 	  {
-        //processing logic goes here
+        //check DSSportal.app and DSSportal.fav against their current states to verify a change occured
+
+        //if its different, build an array of hashes  id/fav/position and send to rails
+      var pageLayout = []
+      var appLayout;
+      var i = 1;
+      $("#sortableFav li").each(function() { 
+        appStructure = {position: i, id: $this.attr('id'), favorite: "true"};      
+        i++;    
+        appLayout.push(appStructure); 
+      });
+
+      i = 0;
+      
+      $("#sortableApp li").each(function() { 
+        appStructure = {position: i, id: $this.attr('id'), favorite: "false"};      
+        i++;    
+        appLayout.push(appStructure); 
+      });
+
+//        $.post("/favorites/drag_create", pobj, function(data) {
+//				$("#" + data.old_id).attr('id', data.new_id);
+//			});
 	  },
 connectWith: ".connectedSortable"
 	});
