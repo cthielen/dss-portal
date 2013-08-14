45ce7268427bd87cbf97af45587f6e77bb991331
Christopher Thielen <cthielen@gmail.com>
2013-08-14 16:17:03 -0700

    Use named scope to simplify ApplicationAssignment#index logic and transfer sorting work to database

--- app/models/application_assignment.rb
+++ app/models/application_assignment.rb
@@ -1,4 +1,7 @@
 class ApplicationAssignment < ActiveRecord::Base
+  scope :favorite, -> { where(favorite: true) }
+  scope :non_favorite, -> { where(favorite: false) }
+  
   belongs_to :person
   attr_accessible :favorite, :person_id, :position, :name, :url, :description, :bookmark, :rm_application_id
 end
