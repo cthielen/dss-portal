35bb58a730654dedc42892e5beda31acd1904a1d
ltwheeler <ltwheeler@ucdavis.edu>
2013-08-29 10:07:23 -0700

    Fixed a bug where the create application bookmark interface was being included with applications for ordering data

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -178,6 +178,7 @@ function SendState()
   var i = 1;
   $("#sortableFav li").each(function() {   
     var id = $(this).attr('id');
+    alert(id);
     appStructure = {position: i, app_id: id, favorite: "true"};      
     i++;    
     pageLayout.push(appStructure); 
@@ -187,9 +188,13 @@ function SendState()
   
   $("#sortableApp li").each(function() { 
     var id = $(this).attr('id');
+    alert(id);
     appStructure = {position: i, app_id: id, favorite: "false"};      
-    i++;    
-    pageLayout.push(appStructure); 
+    i++;
+    if( id.length >= 1)   
+    {
+      pageLayout.push(appStructure); 
+    }
   });
   var pobj = {pageLayout: pageLayout}; 
 
