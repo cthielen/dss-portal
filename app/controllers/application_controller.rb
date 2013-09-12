3dfc93d4b5bc9c8a10c2506a94b301605b10373c
Christopher Thielen <cthielen@gmail.com>
2013-09-12 11:56:35 -0700

    Fixed a bug where newly created app_attributes wouldn't be initialized correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,43 +41,35 @@ class ApplicationController < ActionController::Base
     @rm_apps.each do |app|
       app_assignment = @person.application_assignments.find_or_create_by_rm_application_id(app[:id])
       
-      #Ensure rm_app_data for given app was recently updated
-      app_attribute = RmApplicationAttribute.find_or_create_by_rm_application_id(app[:id])
-      if app_attribute.updated_at < 72.hours.ago
-        #query rm application for particular attributes on the application
+      # Ensure rm_app_data for given app was recently updated
+      app_attribute = RmApplicationAttribute.find_or_initialize_by_rm_application_id(app[:id])
+      
+      if app_attribute.new_record? or app_attribute.updated_at < 72.hours.ago
+        logger.debug "Updating app attribute #{app_attribute.id}/#{app_attribute.name}"
+        
+        # Query rm application for particular attributes on the application
         @app_attribute_data = RmApplication.find(app[:id])
-        #record data into app_attribute
+        # Record data into app_attribute
         app_attribute.name = @app_attribute_data.name
         app_attribute.description = @app_attribute_data.description
         app_attribute.url = @app_attribute_data.url
-#        app_attribute.icon_path = @app_attribute_data.icon_path
-        app_attribute.save
+        #app_attribute.icon_path = @app_attribute_data.icon_path
+        app_attribute.save!
+      else
+        logger.debug "Not updating app attribute #{app_attribute.id}/#{app_attribute.name}"
       end
 
-      # Verifies RM had icon data, otherwise uses default icon
-      # if @rm_app_data.image.present?                           Icon field does not currently exist in RM
-      #   app_assignment.image = app_attribute.image    
-#       else
-#     app.assignment.image = 
-      # end
-
-#      end
-
-      #If url is blank, remove assignment
-#      if app_attribute.url.blank?
-#        app_assignment.destroy
-#      else
-        # Update attributes
-        app_assignment.name = app_attribute.name
-        app_assignment.description = app_attribute.description
-        app_assignment.url = app_attribute.url
-        #if rm doesn't have an icon, use a default
-          first_letter = app_assignment.name.chars.first.downcase
-          icon_path = Icon.find_by_letter(first_letter).image.url
-          app_assignment.image = icon_path
-        app_assignment.save!
-#      end
+      app_assignment.name = app_attribute.name
+      app_assignment.description = app_attribute.description
+      app_assignment.url = app_attribute.url
+      
+      # If RM doesn't specify an icon use a default
+      first_letter = app_assignment.name.chars.first.downcase
+      icon_path = Icon.find_by_letter(first_letter).image.url
+      app_assignment.image = icon_path
+      app_assignment.save!
     end
+
     # Go through @person.application_assignments and remove any non-bookmark ones which are not in @rm_apps
     @person.application_assignments.keep_if do |assignment|
       assignment.bookmark or @rm_apps.find_index{ |r| r[:id] == assignment.rm_application_id }
