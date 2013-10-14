8b3ec73a7cf85c9e6c1604d72509295ab77a5b4d
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2013-10-14 15:20:22 -0700

    Added configuration for whenever into capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,7 @@
 require "bundler/capistrano"
+set :whenever_command, "bundle exec whenever"
+set :whenever_environment, defer { stage }
+require "whenever/capistrano"
 
 after "deploy:restart", "deploy:prime_cache"
 
