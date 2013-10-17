44ae1c47c4ca43bae969f2e16e0646ae36da3a0d
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2013-10-17 16:11:29 -0700

    Sync properly reads messenger rss feed, and updated gems

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,12 @@
 class Message < ActiveRecord::Base
   attr_accessible :description, :guid, :published, :title
-  
+
   def syncgo
-    puts "SYNCGO METHOD CALLED"
-    
+    uri = URI.parse("https://messenger.dss.ucdavis.edu/messages/open.rss")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
+    @response = http.request(Net::HTTP::Get.new(uri.request_uri))
+    puts @response.body
   end
 end
