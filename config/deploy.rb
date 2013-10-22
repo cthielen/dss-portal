4bdab072ec7dede2cd114cc268a3a2e5c304ef3e
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2013-10-22 16:51:53 -0700

    Removing whenever gem and capistrano integration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,4 @@
 require "bundler/capistrano"
-set :whenever_command, "bundle exec whenever"
-set :whenever_environment, defer { stage }
-require "whenever/capistrano"
 
 after "deploy:restart", "deploy:prime_cache"
 
