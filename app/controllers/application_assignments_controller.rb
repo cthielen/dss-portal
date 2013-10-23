5798bb8ffc55014f7cbb027698cd4f79131cfcd6
Christopher Thielen <cthielen@gmail.com>
2013-10-23 13:39:00 -0700

    Avoid duplicate layouts. Code clean ups.

--- app/controllers/application_assignments_controller.rb
+++ app/controllers/application_assignments_controller.rb
@@ -1,20 +1,11 @@
 class ApplicationAssignmentsController < ApplicationController
   filter_resource_access
   respond_to :html, :json
-  before_filter only: [:index] do
-   @messages = Rails.cache.fetch("algo", expires_in: 1.minutes) do
-    currentMessages
-   end
-   logger.info "---------------------------------------------------------------------" 
-   logger.info "---------------------------------------------------------------------" 
-   logger.info @messages
-   logger.info @messages.length
-   logger.info "---------------------------------------------------------------------" 
-  end
   
   def index
     logger.info "#{current_user.log_identifier}@#{request.remote_ip}: Loaded application assignments index (main page)."
     
+    @messages = Rails.cache.fetch("algo", expires_in: 3.minutes) { fetch_messenger_rss }
     current_user.refresh!
     @current_user = current_user
   end
@@ -51,4 +42,34 @@ class ApplicationAssignmentsController < ApplicationController
     end
   end
 
+  private
+  
+  def fetch_messenger_rss
+    require 'rexml/document'
+    
+    # Read DSS Messenger RSS feed for active messages
+    uri = URI.parse("https://messenger.dss.ucdavis.edu/messages/open.rss")
+    
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE # this has to go
+    
+    response = http.request(Net::HTTP::Get.new(uri.request_uri))
+    doc = REXML::Document.new(response.body)
+    
+    messages = []
+    
+    doc.elements.each('rss/channel/item') do |ele|
+      message = Hash.new
+      
+      messages << {
+        'title' => ele.elements['title'].text,
+        'description' => ele.elements['description'].text,
+        'published' => ele.elements['pubDate'].text,
+        'guid' => ele.elements['guid'].text
+      }
+    end
+    
+    return messages
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,46 +15,15 @@ class ApplicationController < ActionController::Base
     Authorization.current_user = current_user
   end
   
-  # Handling errors
+  # Handle RM unavailbility
   class RmUnreachable < StandardError; end
   rescue_from RmUnreachable, :with => :render_rm_unreachable
 
   def render_rm_unreachable
     respond_to do |format| 
-      format.html { render :template => "errors/tech_difficulties", :status => 500, :layout => "error" } 
+      format.html { render :template => "errors/tech_difficulties", :status => 500 } 
     end
-    true
-  end
     
-  def currentMessages
-    require 'rexml/document'
-
-    # read dss-messeenger rss feed for 'most recent messages''
-    uri = URI.parse("https://messenger.dss.ucdavis.edu/messages/open.rss")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE # read into this
-    response = http.request(Net::HTTP::Get.new(uri.request_uri))
-    xml_data = response.body
-    doc = REXML::Document.new(xml_data)
-    titles = []
-    links = []
-
-    messages = Array.new
-    message = Hash.new
-    # populate messages array
-    doc.elements.each('rss/channel/item') do |ele|
-       title = ele.elements['title'].text
-       description = ele.elements['description'].text
-       published = ele.elements['pubDate'].text
-       guid = ele.elements['guid'].text
-       
-       message['title'] = title
-       message['description'] = description
-       message['published'] = published
-       message['guid'] = guid
-       messages << message
-    end
-    messages
+    true
   end
 end
