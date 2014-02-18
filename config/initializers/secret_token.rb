d05e420ad03978618426a37e181de02f4b549035
Christopher Thielen <cthielen@gmail.com>
2014-02-18 11:15:20 -0800

    Removed secret token from repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DssPortal::Application.config.secret_token = '206efac36f48ee51ad76d2e2df66c545773c238c9d206fa2e92d094a89c49f5491a41145288cfd46a42c4d9efac7292790fb1a4464fb5a31b70620cfd2b3b3e6'
+# You can use `rake secret` to generate a secure secret key.
+
+SECRET_TOKEN_FILE = Rails.root.join( "config", "secret_token.yml")
+
+if File.file?(SECRET_TOKEN_FILE)
+  SECRET_TOKEN_SETTINGS = YAML.load_file(SECRET_TOKEN_FILE)
+  DssPortal::Application.config.secret_token = SECRET_TOKEN_SETTINGS["SECRET_TOKEN"]
+else
+  puts "You need to set up config/secret_token.yml before running this application."
+  exit
+end
